c2d8369a0bbe0dfed067a9271d43572a378511d1
wenke.lai <wenke.studio@gmail.com>
2025-12-23 14:02:10 +0800

    feat: initial dev page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { Header } from '$lib/components/header';
 	import './layout.css';
 
 	const { children } = $props();
 </script>
+
+<Header>this is header</Header>
 
 {@render children()}
