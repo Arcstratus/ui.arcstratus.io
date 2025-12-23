c619ccbc1ac810d3b1bb91aa56d4bcbe50091890
wenke.lai <wenke.studio@gmail.com>
2025-12-23 12:47:32 +0800

    feat(header): add header component

---
+++ src/lib/components/header/header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" module>
+	import type { Snippet } from 'svelte';
+
+	interface HeaderProps {
+		children: Snippet;
+	}
+</script>
+
+<script lang="ts">
+	const { children }: HeaderProps = $props();
+</script>
+
+<header class="fixed inset-x-0 top-0 z-50">
+	<div class="container mx-auto h-16 overflow-hidden">
+		{@render children()}
+	</div>
+</header>
+
+<div class="h-16"></div>
